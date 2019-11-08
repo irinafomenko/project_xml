41a20e20bf56ecf3cfbde8f19bbe3c2850d838be
Fomenko Irina <irina.fomenko@mera.com>
2019-11-08 13:05:09 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/project_xml.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_xml.dir/main.cpp.o"
+  "project_xml.pdb"
+  "project_xml.exe"
+  "libproject_xml.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_xml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/project_xml.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for project_xml.
+# This may be replaced when dependencies are built.
